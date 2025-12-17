6e66bd1d0e6ba0f20f244b85adcee00c506de7ac
Matt Russell <MattRussellUK@gmail.com>
2025-12-17 16:45:22 +0000

    wip

--- lib/puzzles.ts
+++ lib/puzzles.ts
@@ -36,26 +36,26 @@ export interface Puzzle {
 }
 
 export const puzzles: Record<PuzzleId, Puzzle> = {
-  [PuzzleId(67)]: {
-    id: PuzzleId(67),
+  [PuzzleId(42)]: {
+    id: PuzzleId(42),
     date: Iso8601Date("2026-06-10"),
-    speciesId: SpeciesId(2), // Tansy
-    observationDate: Iso8601Date("2023-08-03"),
+    speciesId: SpeciesId(11), // Bird's-eye Primrose
+    observationDate: Iso8601Date("2024-05-18"),
     location: "North Yorkshire, England",
-    coordinates: { lat: Degrees(53.9788528), lng: Degrees(-1.3101916) },
-    habitat: "Flood meadow",
+    coordinates: { lat: Degrees(54.2015), lng: Degrees(-2.3500028) },
+    habitat: "Former limestone quarry",
     images: [
-      { url: Url("/images/67/plant.jpg"), caption: "Whole plant" },
-      { url: Url("/images/67/flower-closeup.jpg"), caption: "Flower heads" },
-      { url: Url("/images/67/leaves.jpg"), caption: "Leaves" },
+      { url: Url("/images/42/plant.jpg"), caption: "Whole plant" },
+      { url: Url("/images/42/close-up-of-flowers.jpg"), caption: "Close-up of flowers" },
+      { url: Url("/images/42/leaves.jpg"), caption: "Leaves" },
     ],
     photoAttribution: {
       photographer: "Matt Russell",
       license: "CC-BY 4.0",
     },
   },
-  [PuzzleId(68)]: {
-    id: PuzzleId(68),
+  [PuzzleId(43)]: {
+    id: PuzzleId(43),
     date: Iso8601Date("2026-06-11"),
     speciesId: SpeciesId(1), // Devil's-bit Scabious
     observationDate: Iso8601Date("2025-08-13"),
@@ -63,28 +63,28 @@ export const puzzles: Record<PuzzleId, Puzzle> = {
     coordinates: { lat: Degrees(55.2267806), lng: Degrees(-2.5802806) },
     habitat: "Woodland",
     images: [
-      { url: Url("/images/68/flower-head.jpg"), caption: "Flower head" },
-      { url: Url("/images/68/flower-rear.jpg"), caption: "Rear view of flower" },
-      { url: Url("/images/68/flower-closeup.jpg"), caption: "Close up of flower head" },
-      { url: Url("/images/68/leaves.jpg"), caption: "Leaves" },
+      { url: Url("/images/43/flower-head.jpg"), caption: "Flower head" },
+      { url: Url("/images/43/flower-rear.jpg"), caption: "Rear view of flower" },
+      { url: Url("/images/43/flower-closeup.jpg"), caption: "Close up of flower head" },
+      { url: Url("/images/43/leaves.jpg"), caption: "Leaves" },
     ],
     photoAttribution: {
       photographer: "Matt Russell",
       license: "CC-BY 4.0",
     },
   },
-  [PuzzleId(69)]: {
-    id: PuzzleId(69),
+  [PuzzleId(44)]: {
+    id: PuzzleId(44),
     date: Iso8601Date("2026-06-12"),
-    speciesId: SpeciesId(11), // Bird's-eye Primrose
-    observationDate: Iso8601Date("2024-05-18"),
+    speciesId: SpeciesId(2), // Tansy
+    observationDate: Iso8601Date("2023-08-03"),
     location: "North Yorkshire, England",
-    coordinates: { lat: Degrees(54.2015), lng: Degrees(-2.3500028) },
-    habitat: "Former limestone quarry",
+    coordinates: { lat: Degrees(53.9788528), lng: Degrees(-1.3101916) },
+    habitat: "Flood meadow",
     images: [
-      { url: Url("/images/69/plant.jpg"), caption: "Whole plant" },
-      { url: Url("/images/69/close-up-of-flowers.jpg"), caption: "Close-up of flowers" },
-      { url: Url("/images/69/leaves.jpg"), caption: "Leaves" },
+      { url: Url("/images/44/plant.jpg"), caption: "Whole plant" },
+      { url: Url("/images/44/flower-closeup.jpg"), caption: "Flower heads" },
+      { url: Url("/images/44/leaves.jpg"), caption: "Leaves" },
     ],
     photoAttribution: {
       photographer: "Matt Russell",
